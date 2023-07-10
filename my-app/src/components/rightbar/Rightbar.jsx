4e30d24a891ad446054bc2a3b3cb1d422cfc7498
lindiyip <lindiyip1997@hotmail.com>
2023-07-10 12:02:31 +0800

    react mainframe

---
+++ my-app/src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,37 @@
+import "./rightbar.css";
+
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return <>original content</>;
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User Information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">Singapore</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Singapore</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <ProfileRightbar />
+      </div>
+    </div>
+  );
+}
---
+++ myapp/src/pages/profile/Profile.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./profile.css";
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className=" profileCoverImg"
+                src="assets/background.jpeg"
+                alt=""
+              />
+              <img
+                className=" profileUserImg"
+                src="assets/Portrait.jpg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Lindi Yip</h4>
+              <span className="profileInfoDesc">Hello my friends</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
